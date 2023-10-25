59f559883930cebce377884c598d5f80c05c512c
micahael123321123 <d1104181048@gm.lhu.edu.tw>
2023-10-25 13:56:00 +0800

    first commit

--- database/migrations/2023_10_18_021233_create_members_table.php
+++ database/migrations/2023_10_18_021233_create_members_table.php
@@ -14,8 +14,17 @@ return new class extends Migration
     public function up()
     {
         Schema::create('members', function (Blueprint $table) {
-            $table->id();
+            $table->id()->comment('編號');
+            $table->string("name")->comment('人員名稱');
+            $table->integer("oid")->unsigned()->comment('團體');
+            $table->string("position")->comment('位置');
+            $table->float("height")->unsigned()->comment('身高');
+            $table->float("weight")->unsigned()->comment('體重');
+            $table->integer("year")->unsigned()->comment('年資');
+            $table->integer("age")->unsigned()->comment('年齡');
+            $table->string("nationality")->comment('國籍');
             $table->timestamps();
+            
         });
     }
 
--- database/migrations/2023_10_18_021330_create_orchestra_table.php
+++ database/migrations/2023_10_18_021330_create_orchestra_table.php
@@ -15,9 +15,13 @@ return new class extends Migration
     {
         Schema::create('orchestra', function (Blueprint $table) {
             $table->id();
+            $table->string("name")->comment('團體名稱');
+            $table->string("company")->comment('公司名稱');
+            $table->string("city")->comment('公司位置');
+            $table->string("style")->comment('曲風類別');
             $table->timestamps();
         });
-    }
+    } 
 
     /**
      * Reverse the migrations.
